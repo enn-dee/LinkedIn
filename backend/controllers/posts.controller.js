e6538231c1d94486f7cfb3b097e866d8177b3e64
enn-dee <nddar191131@gmail.com>
2025-01-17 18:27:00 +0530

    Add post routes and controllers
    - Set up routes for feed, create, delete, like, and comment on posts.
    - Secured routes with `protectedRoute` middleware.
    - Connected routes to corresponding post controllers.

---
+++ backend/controllers/posts.controller.js
@@ -0,0 +1,156 @@
+import cloudinary from "../lib/cloudinary.js";
+import Post from "../models/posts.model.js";
+
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({ author: { $in: req.user.connections } })
+      .populate("author", "name username profileProfile headline")
+      .populate("comments.user", "name profilePicture")
+      .sort({ createdAt: -1 });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("error in getfeedposts : ", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const { content, image } = req.body;
+    let newPost;
+    if (image) {
+      const imgResult = await cloudinary.uploader.upload(image);
+
+      newPost = new Post({
+        author: req.user._id,
+        content,
+        image: imgResult.secure_url,
+      });
+    } else {
+      newPost = new Post({
+        author: req.user._id,
+        content,
+      });
+    }
+    await newPost.save();
+    return res.status(201).json(newPost);
+  } catch (error) {
+    console.log("error in createPost: ", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    // can't delete , bcz u r not author of this post
+    if (post.author.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this post" });
+    }
+    if (post.image) {
+      // delete image from cloudinary
+      await cloudinary.uploader.destroy(
+        post.image.split("/").pop().split(".")[0]
+      );
+    }
+    await Post.findByIdAndDelete(postId);
+    return res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("error in deletePost: ", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId)
+      .populate("author", "name username profilePicture headline")
+      .populate("comments.user", "name profilePicture");
+    res.status(200).json(port);
+  } catch (error) {
+    console.log("Error in getPostById ", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const createComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { content } = req.body;
+    const post = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $push: { comments: { user: req.user._id, content } },
+      },
+      { new: true }
+    ).populate("author", "name email username headline profilePicture");
+    // notifi. if comment owner is not the post owner
+    if (post.author.toString() !== req.user._id) {
+      const newNotification = new Notification({
+        recipient: post.author,
+        type: "comment",
+        relatedUser: req.user._id,
+        relatedPost: postId,
+      });
+      await newNotification.save();
+      //   send email
+
+      try {
+        const postUrl = process.env.CLIENT_URL + "/post/" + postId;
+        await sendCommentNotificationEmail(
+          post.author.email,
+          post.author.name,
+          req.user.name,
+          postUrl,
+          content
+        );
+      } catch (error) {
+        console.log("failed to send email: createComment func: ", error);
+      }
+    }
+    return res.status(200).json(post);
+  } catch (error) {
+    console.log("Error in createComment: ", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const likePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const post = await Post.findById(postId);
+    // means user is unliking post [already liked]
+    if (post.likes.included(userId)) {
+      post.likes = post.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+    } else {
+      // user liking the post
+      post.likes.push(userId);
+      //   notification gen. kro agr post ko like karne wala ,  owner nhi hai toh
+      if (post.author.toString() !== userId.toString()) {
+        const newNotification = new Notification({
+          recipient: post.author,
+          type: "like",
+          relatedUser: userId,
+          relatedPost: postId,
+        });
+        await newNotification.save();
+      }
+    }
+    await post.save();
+    return res.status(200).json(post);
+  } catch (error) {
+    console.log("error in like post: ", error);
+    return res.status(500).json({ message: "server error" });
+  }
+};
---
+++ backend/routes/post.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
+import {
+    createComment,
+  createPost,
+  deletePost,
+  getFeedPosts,
+  getPostById,
+  likePost
+} from "../controllers/posts.controller.js";
+const router = express.Router();
+
+router.get("/", protectedRoute, getFeedPosts);
+router.post("/create", protectedRoute, createPost);
+router.post("/delete/:id", protectedRoute, deletePost);
+router.get("/:id", protectedRoute, getPostById);
+router.post("/:id/comment", protectedRoute, createComment);
+router.post("/:id/like", protectedRoute, likePost);
+
+export default router;
