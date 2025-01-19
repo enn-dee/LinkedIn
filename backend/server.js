3d7c4ea032dc9ebbf26d5e81a966878090368137
enn-dee <nddar191131@gmail.com>
2025-01-19 19:10:40 +0530

    add: Cors allowance for origin react (path)  and credentials (to get cookie set in backend)

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-import connectionRoutes from "./routes/connection.route.js"
+import connectionRoutes from "./routes/connection.route.js";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -13,9 +14,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
